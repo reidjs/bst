77170885353f2d89a282fd7a7585153f5359c9e4
reid <reidsherman@gmail.com>
2018-01-06 14:28:28 -0800

    Insert and find work on BST

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,6 +1,7 @@
 # There are many ways to implement these methods, feel free to add arguments 
 # to methods as you see fit, or to create helper methods.
 require_relative 'bst_node'
+require 'byebug'
 class BinarySearchTree
   attr_accessor :root
   def initialize
@@ -11,12 +12,28 @@ class BinarySearchTree
     if @root.nil?
       @root = BSTNode.new(value) 
     else 
-      find(value)
+      position_found = false 
+      node = @root
+      until node.nil?
+        current_position = nil 
+        if value <= node.value 
+          node.left.nil? ? node.left = BSTNode.new(value) : current_position = node.left
+        else 
+          node.right.nil? ? node.right = BSTNode.new(value) : current_position = node.right  
+        end 
+        node = current_position
+      end 
     end 
   end
 
+
   def find(value, tree_node = @root)
-    
+    return nil if tree_node.nil?
+    return tree_node if value == tree_node.value 
+    byebug
+    return find(value, tree_node.left) if value < tree_node.value 
+    return find(value, tree_node.right) if value > tree_node.value 
+
   end
 
   def delete(value)
@@ -40,3 +57,12 @@ class BinarySearchTree
   # optional helper methods go here:
 
 end
+
+bst = BinarySearchTree.new
+bst.insert(5)
+bst.insert(3)
+bst.insert(1)
+bst.insert(8)
+bst.insert(7)
+
+# p bst.find(7)
